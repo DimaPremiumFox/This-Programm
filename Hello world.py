89cf46f12931ad93084e943ddc7ff1dbedd20443
DimaRisen <killtrchannel@gmail.com>
2020-08-15 15:42:57 +0300

    Name and now

--- Hello world.py
+++ Hello world.py
@@ -1,3 +1,10 @@
 print("Hello world")
 name = input("Как тебя зовут? ")
 print(" Привет " + name + " Добрый день ")
+while True:
+    print(""" 
+    1.Яндекс
+    2.Гугл
+    3.Дискорд
+    """)
+    now = input("Что сейчас будем делать? ") 
