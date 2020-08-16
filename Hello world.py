00e67c69a92b294f764cff18f419ae15f6aa98d9
DimaRisen <killtrchannel@gmail.com>
2020-08-16 11:27:09 +0300

    Был добавлен англ язык и нормальное закрытие программы
    В этом обновлении добавил английския язык и нормальное закрытие программы.

--- Hello world.py
+++ Hello world.py
@@ -1,4 +1,51 @@
+import sys
+import winsound
+Freq = 2500
+Dur = 900
+winsound.Beep(Freq,Dur)
 print("Hello world")
+print("You who?")
+laungage = input("You Russian or English?: ")
+if laungage == "English":
+    print("Hello")
+    namme = input("What's your name? ")
+    print("Hi " + namme + " good day ")
+    print("""This is a list of what you can find out about, just write a number and you will find out 
+    1.Yandex
+    2.Google
+    3.Discord
+    4.Malwarebytes
+    5.Vivaldi
+    6.Brave
+    7.Steam
+    8.AnyDesk
+    """)
+    print("please write ---help")
+    while True:
+        noww = input("What are we going to do or find out now? ")
+        if noww == "1.":
+            print("Yandex is a Russian company that created Yandex browser and many other services.")
+        elif noww == "2.":
+            print("Google is the most popular company in the world and the most profitable. Also known for the massive Google Chrome browser that brought super changes to the world!")
+        elif noww == "3.":
+            print("This program is designed to communicate like Skype.")
+        elif noww == "4.":
+            print("Malwarebytes is an antivirus and in my humble opinion it is the best antivirus")
+        elif noww == "5.":
+            print("Vivaldi is almost an opera browser. When one developer left the opera company he wanted to create his own browser and created Vivaldi!")
+        elif noww == "6.":
+            print("Brave is a popular browser. It is like chrome and a fox muff. And it is fast as vivaldi and does not use all resources.")
+        elif noww == "7.":
+            print("Steam is an online service for the digital distribution of computer games and software developed and maintained by Valve.")
+        elif noww == "8.":
+            print("AnyDesk is a program written in c ++ and also that would remotely control a computer or help a person. I recommend to everyone!")
+        elif noww == "---help":
+            print("List")
+            print("quit is for quit")
+        elif noww == "quit":
+            sys.exit()
+        else:
+            print("There is no such command")                
 name = input("Как тебя зовут? ")
 print(" Привет " + name + " Добрый день ")
 print("""Это список о чём можно узнать просто напиши цифру и ты узнаешь 
@@ -39,6 +86,6 @@ while True:
     elif now == "nerbi":
         print("Нерби спасибо огромное тебе так как я знаю то что ты читаешь!Ты самый лучший мой друг!Скоро тебе дам гта 5)))")    
     elif now == "exit":
-        break
+        sys.exit()
     else:
         print("Нет такой команды")    
